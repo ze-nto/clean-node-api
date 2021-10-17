8a3c26aad4cf3d9613fc5151e0de5dfc997ec2ea
Jose Pereira <josepsneto@gmail.com>
2021-10-17 19:07:49 -0300

    Ensure LoginRouter calls AuthUseCase with correct params

--- src/presentation/routers/loginRouter.test.js
+++ src/presentation/routers/loginRouter.test.js
@@ -2,12 +2,24 @@ const LoginRouter = require('./LoginRouter')
 const MissingParamError = require('../helpers/MissingParamError')
 
 const makeSut = () => {
-  return new LoginRouter()
+  class AuthUseCaseSpy {
+    auth (email, password) {
+      this.email = email
+      this.password = password
+    }
+  }
+
+  const authUseCaseSpy = new AuthUseCaseSpy()
+  const sut = new LoginRouter(authUseCaseSpy)
+  return {
+    sut,
+    authUseCaseSpy
+  }
 }
 
 describe('Login Router', () => {
   it('should return 400 if no email is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -19,7 +31,7 @@ describe('Login Router', () => {
   })
 
   it('should return 400 if no password is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any@email.com'
@@ -30,14 +42,27 @@ describe('Login Router', () => {
   })
 
   it('should return 500 if no httpRequest is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpResponse = sut.route()
     expect(httpResponse.statusCode).toBe(500)
   })
 
   it('should return 500 if no httpRequest has no body', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpResponse = sut.route({})
     expect(httpResponse.statusCode).toBe(500)
+  })
+
+  it('should call AuthUseCase with correct params', () => {
+    const { sut, authUseCaseSpy } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any@example.com',
+        password: 'any_pass'
+      }
+    }
+    sut.route(httpRequest)
+    expect(authUseCaseSpy.email).toBe(httpRequest.body.email)
+    expect(authUseCaseSpy.password).toBe(httpRequest.body.password)
   })
 })

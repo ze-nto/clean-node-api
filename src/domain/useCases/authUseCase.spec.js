530f2caf92a66c567878fad94dcc332794546254
Jose Pereira <josepsneto@gmail.com>
2021-10-30 18:26:00 -0300

    Ensures AuthUseCase returns null if LoadUserByEmailRepository return null

--- src/domain/useCases/authUseCase.spec.js
+++ src/domain/useCases/authUseCase.spec.js
@@ -19,7 +19,10 @@ class AuthUseCase {
     if (!this.loadUserByEmailRepository.load) {
       throw new InvalidParamError('loadUserByEmailRepository')
     }
-    await this.loadUserByEmailRepository.load(email)
+    const user = await this.loadUserByEmailRepository.load(email)
+    if (!user) {
+      return null
+    }
   }
 }
 
@@ -66,5 +69,11 @@ describe('AuthUseCase', () => {
     const sut = new AuthUseCase({})
     const promise = sut.auth('any_email@email.com', 'any_password')
     expect(promise).rejects.toThrow(new InvalidParamError('loadUserByEmailRepository'))
+  })
+
+  it('Should reurn null if LoadUserByEmailRepository returns null', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth('invalid_email@email.com', 'any_password')
+    expect(accessToken).toBeNull()
   })
 })

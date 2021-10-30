7d87cf7ba926801921735e67a6afe532834ed310
Jose Pereira <josepsneto@gmail.com>
2021-10-30 17:37:40 -0300

    Ensures AuthUseCase throws if no email is provided

---
+++ src/domain/useCases/authUseCase.spec.js
@@ -0,0 +1,17 @@
+/* eslint-env jest */
+
+class AuthUseCase {
+  async auth (email) {
+    if (!email) {
+      throw new Error()
+    }
+  }
+}
+
+describe('Auth UseCase', () => {
+  it('Should throw if no email is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth()
+    expect(promise).rejects.toThrow()
+  })
+})

05167c9b403d6222876afdce15c2249a9c19bae6
Jose Pereira <josepsneto@gmail.com>
2021-10-30 18:21:40 -0300

    Ensures AuseUseCase throws if an invalid LoadUserByEmailRepository is provided

--- src/domain/useCases/authUseCase.spec.js
+++ src/domain/useCases/authUseCase.spec.js
@@ -1,5 +1,5 @@
 /* eslint-env jest */
-const { MissingParamError } = require('../../utils/errors')
+const { MissingParamError, InvalidParamError } = require('../../utils/errors')
 
 class AuthUseCase {
   constructor (loadUserByEmailRepository) {
@@ -12,6 +12,12 @@ class AuthUseCase {
     }
     if (!password) {
       throw new MissingParamError('password')
+    }
+    if (!this.loadUserByEmailRepository) {
+      throw new MissingParamError('loadUserByEmailRepository')
+    }
+    if (!this.loadUserByEmailRepository.load) {
+      throw new InvalidParamError('loadUserByEmailRepository')
     }
     await this.loadUserByEmailRepository.load(email)
   }
@@ -31,7 +37,7 @@ const makeSut = () => {
   }
 }
 
-describe('Auth UseCase', () => {
+describe('AuthUseCase', () => {
   it('Should throw if no email is provided', async () => {
     const { sut } = makeSut()
     const promise = sut.auth()
@@ -48,5 +54,17 @@ describe('Auth UseCase', () => {
     const { sut, loadUserByEmailRepositorySpy } = makeSut()
     await sut.auth('any_email@email.com', 'any_password')
     expect(loadUserByEmailRepositorySpy.email).toBe('any_email@email.com')
+  })
+
+  it('Should throw if no LoadUserByEmailRepository is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any_email@email.com', 'any_password')
+    expect(promise).rejects.toThrow(new MissingParamError('loadUserByEmailRepository'))
+  })
+
+  it('Should throw if LoadUserByEmailRepository has no load method', async () => {
+    const sut = new AuthUseCase({})
+    const promise = sut.auth('any_email@email.com', 'any_password')
+    expect(promise).rejects.toThrow(new InvalidParamError('loadUserByEmailRepository'))
   })
 })
